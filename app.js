04787bde001d80223d1fbef8c859d0a61f7a187d
ahmcho <mail@ahmcho.com>
2020-05-24 14:18:21 +0400

    added logout

--- app.js
+++ app.js
@@ -40,6 +40,9 @@ app.use(session({
   saveUninitialized: true
 }))
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
